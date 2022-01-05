fb7c7d0bb30cf0b21f9bf6bf462a6cdd19585fda
liefwerk <natjageni@gmail.com>
2022-01-04 22:02:56 -0300

    Changed hero image and added education and footer parts

---
+++ src/_data/education.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{ 
+		school: 'ISME School',
+		period: '2017 - 2019',
+		degree: 'Master en Management de Projet Web & Digital',
+		description: 'I had the opportunity of improving my SEO, Web Marketing and Web Development skills - but most of all to learn Web project Management. I participated in a handful of projects, ranging from project management to communication, to digital marketing and web development.'
+	},
+	{ 
+		school: 'Sup de Co La Rochelle',
+		period: '2012 - 2015',
+		degree: 'Bachelor Business',
+		description: 'During these three years, I developed skills in communication, web marketing and community management. I was lucky enough to study abroad with Erasmus. This tremendously helped my English skills.'
+	}
+]
