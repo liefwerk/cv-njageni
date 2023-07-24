d08583fef8b76dd85dd79b67e6ea5aea38b5444d
liefwerk <natjageni@gmail.com>
2023-07-24 11:19:18 +0200

    switch some languages and fixed some text descriptions

--- src/_data/competence.js
+++ src/_data/competence.js
@@ -1,16 +1,16 @@
 module.exports = {
   languages: [
+    { name: 'css3', src: '/icons/css3.png' },
     { name: 'javascript', src: '/icons/js.png' },
     { name: 'typescript', src: '/icons/typescript.png' },
     { name: 'python', src: '/icons/python.png' },
-    { name: 'ruby', src: '/icons/ruby.png' },
     { name: 'php', src: '/icons/php.png' },
   ],
   frameworks: [
     { name: 'vue js', src: '/icons/vuejs.png' },
     { name: 'react', src: '/icons/react.png' },
+    { name: 'laravel', src: '/icons/laravel.png' },
     { name: 'wordpress', src: '/icons/wordpress.png' },
     { name: 'woocommerce', src: '/icons/woocommerce.png' },
-    { name: 'laravel', src: '/icons/laravel.png' },
   ]
 }
